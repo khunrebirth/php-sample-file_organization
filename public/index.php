4f5d16a06d6fc55c982f58363d9e99064668d3bd
Thanu Sinlert <khunrebirth@gmail.com>
2017-10-05 22:42:53 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -9,13 +9,15 @@
   	<!-- FONT AWESOME -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<!-- BULMA CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.1/css/bulma.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.1/css/bulma.min.css">
 </head>
 <body>
+	
   <?php 
     include('../app/views/header.php');
     include('../app/views/content.php');
     include('../app/views/footer.php'); 
   ?>
+	
 </body>
 </html>
