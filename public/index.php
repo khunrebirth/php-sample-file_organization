9dda4fe441e8ff85f0810bc333b0b14d4711ad75
Thanu Sinlert <khunrebirth@gmail.com>
2017-10-05 22:40:02 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-  <!-- FONT AWESOME -->
+  	<!-- FONT AWESOME -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<!-- BULMA CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.1/css/bulma.min.css">
